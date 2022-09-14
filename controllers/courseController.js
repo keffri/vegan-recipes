f4a2902f31288bdf4f553abc95d53db1f3230600
keffri <keffri.w.neal@gmail.com>
2022-09-14 08:53:13 -0700

    course list get functionality

--- controllers/courseController.js
+++ controllers/courseController.js
@@ -1,8 +1,20 @@
 const Course = require('../models/course');
 
 // Display list of all courses.
-exports.course_list = (req, res) => {
-  res.send('NOT IMPLEMENTED: Course list');
+exports.course_list = (req, res, next) => {
+  Course.find({}, 'name')
+    .sort({ name: 1 })
+    .populate('name')
+    .exec(function (err, list_courses) {
+      if (err) {
+        return next(err);
+      }
+
+      res.render('course_list', {
+        title: 'Course List',
+        course_list: list_courses,
+      });
+    });
 };
 
 // Display detail page for a specific course.
