e9a1f659d7fb91969c5f7a9fd78d63a7150b5878
keffri <keffri.w.neal@gmail.com>
2022-09-14 08:53:27 -0700

    cuisine list get functionality

--- controllers/cuisineController.js
+++ controllers/cuisineController.js
@@ -1,8 +1,20 @@
 const Cuisine = require('../models/cuisine');
 
 // Display list of all cuisines.
-exports.cuisine_list = (req, res) => {
-  res.send('NOT IMPLEMENTED: Cuisine list');
+exports.cuisine_list = (req, res, next) => {
+  Cuisine.find({}, 'name country')
+    .sort({ name: 1 })
+    .populate('name')
+    .exec(function (err, list_cuisines) {
+      if (err) {
+        return next(err);
+      }
+
+      res.render('cuisine_list', {
+        title: 'Cuisine List',
+        cuisine_list: list_cuisines,
+      });
+    });
 };
 
 // Display detail page for a specific cuisine.
