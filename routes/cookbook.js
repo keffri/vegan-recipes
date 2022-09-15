a997746fdfa2248217ac576310729d1d5ea25e78
keffri <keffri.w.neal@gmail.com>
2022-09-15 10:44:17 -0700

    create get/post routes for each collection

--- routes/cookbook.js
+++ routes/cookbook.js
@@ -11,17 +11,29 @@ router.get('/', recipe_controller.index);
 
 router.get('/recipes', recipe_controller.recipe_list);
 
+router.get('/recipe/create', recipe_controller.recipe_create_get);
+
+router.post('/recipe/create', recipe_controller.recipe_create_post);
+
 router.get('/recipe/:id', recipe_controller.recipe_detail);
 
 // COURSE ROUTES
 
 router.get('/courses', course_controller.course_list);
 
+router.get('/course/create', course_controller.course_create_get);
+
+router.post('/course/create', course_controller.course_create_post);
+
 router.get('/course/:id', course_controller.course_detail);
 
 // CUISINE ROUTES
 
 router.get('/cuisines', cuisine_controller.cuisine_list);
+
+router.get('/cuisine/create', cuisine_controller.cuisine_create_get);
+
+router.post('/cuisine/create', cuisine_controller.cuisine_create_post);
 
 router.get('/cuisine/:id', cuisine_controller.cuisine_detail);
 
