ad895b4a6673d97dc9f0a78e08b3fbda5fd7e2ba
keffri <keffri.w.neal@gmail.com>
2022-09-16 11:02:56 -0700

    recipe get/post update functionality

--- controllers/recipeController.js
+++ controllers/recipeController.js
@@ -104,8 +104,6 @@ exports.recipe_create_post = [
   body('totalTime'),
   body('servingSize'),
   body('link').isURL().withMessage('Invalid URL'),
-  // body('course'),
-  // body('cuisine'),
 
   (req, res, next) => {
     const errors = validationResult(req);
@@ -202,3 +200,124 @@ exports.recipe_delete_post = (req, res, next) => {
     }
   );
 };
+
+exports.recipe_update_get = (req, res, next) => {
+  async.parallel(
+    {
+      recipe(callback) {
+        Recipe.findById(req.params.id)
+          .populate('course')
+          .populate('cuisine')
+          .exec(callback);
+      },
+      courses(callback) {
+        Course.find(callback);
+      },
+      cuisines(callback) {
+        Cuisine.find(callback);
+      },
+    },
+    (err, results) => {
+      if (err) {
+        return next(err);
+      }
+
+      if (results.recipe === null) {
+        const err = new Error('Recipe not found');
+        err.status = 404;
+        return next(err);
+      }
+
+      for (const cuisine of results.cuisines) {
+        for (const recipeCuisine of [results.recipe.cuisine]) {
+          if (cuisine._id.toString() === recipeCuisine._id.toString()) {
+            cuisine.checked = 'true';
+          }
+        }
+      }
+
+      res.render('recipe_form', {
+        title: 'Update Recipe',
+        courses: results.courses,
+        cuisines: results.cuisines,
+        recipe: results.recipe,
+      });
+    }
+  );
+};
+
+exports.recipe_update_post = [
+  (req, res, next) => {
+    if (!Array.isArray(req.body.cuisine)) {
+      req.body.cuisine =
+        typeof req.body.cuisine === 'undefined' ? [] : [req.body.cuisine];
+    }
+    next();
+  },
+  body('name')
+    .trim()
+    .isLength({ min: 1 })
+    .escape()
+    .withMessage('A recipe name must be specified.'),
+  body('prepTime'),
+  body('cookTime'),
+  body('totalTime'),
+  body('servingSize'),
+  body('link').isURL().withMessage('Invalid URL'),
+  (req, res, next) => {
+    const errors = validationResult(req);
+
+    const recipe = new Recipe({
+      name: req.body.name,
+      prepTime: req.body.prepTime,
+      cookTime: req.body.cookTime,
+      totalTime: req.body.totalTime,
+      servingSize: req.body.servingSize,
+      link: req.body.link,
+      course: req.body.course,
+      cuisine: typeof req.body.cuisine === 'undefined' ? [] : req.body.cuisine,
+      _id: req.params.id,
+    });
+
+    if (!errors.isEmpty()) {
+      async.parallel(
+        {
+          courses(callback) {
+            Course.find(callback);
+          },
+          cuisines(callback) {
+            Cuisine.find(callback);
+          },
+        },
+        (err, results) => {
+          if (err) {
+            return next(err);
+          }
+
+          for (const cuisine of results.cuisines) {
+            if (recipe.cuisine.includes(genre._id)) {
+              cuisine.checked = 'true';
+            }
+          }
+
+          res.render('recipe_form', {
+            title: 'Update Recipe',
+            courses: results.courses,
+            cuisines: results.cuisines,
+            recipe,
+            errors: errors.array(),
+          });
+        }
+      );
+      return;
+    }
+
+    Recipe.findByIdAndUpdate(req.params.id, recipe, {}, (err, therecipe) => {
+      if (err) {
+        return next(err);
+      }
+
+      res.redirect(therecipe.url);
+    });
+  },
+];
